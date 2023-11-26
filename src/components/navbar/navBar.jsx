497963b8678d08cf1ea9f197374f86ecff963660
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2023-11-26 00:46:28 -0500

    adding active page highlight in nav

--- src/components/navbar/navBar.jsx
+++ src/components/navbar/navBar.jsx
@@ -1,14 +1,13 @@
 import { AiOutlineMenu } from "react-icons/ai";
 import { AiOutlineClose } from "react-icons/ai";
-
 import { useState } from "react";
-import "./navbar.css"
+import "./navbar.css";
 import { Link } from 'react-router-dom';
 
-const NavItem = ({ to, children, onClick }) => {
+const NavItem = ({ to, children, onClick, isActive }) => {
   return (
     <li>
-      <Link to={to} onClick={onClick}>
+      <Link to={to} onClick={onClick} className={isActive ? 'active' : ''}>
         {children}
       </Link>
     </li>
@@ -17,36 +16,43 @@ const NavItem = ({ to, children, onClick }) => {
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
+  const [activeNavItem, setActiveNavItem] = useState(null);
+
   const handleNav = () => {
     setNav(!nav);
   };
 
+  const handleNavItemClick = (to) => {
+    setActiveNavItem(to);
+    handleNav();
+  };
+
   return (
     <>
-      <nav >
+      <nav>
         <Link to="/" className="logo">
           baking horizon
         </Link>
         <ul className="top-ul">
-          <NavItem to="/">home</NavItem>
-          <NavItem to="/gallery">gallery</NavItem>
-          <NavItem to="/menu">menu</NavItem>
-          <NavItem to="/faq">faq</NavItem>
-          <NavItem to="/contact">contact</NavItem>
+          <NavItem to="/" onClick={() => handleNavItemClick("/")} isActive={activeNavItem === "/"}>home</NavItem>
+          <NavItem to="/gallery" onClick={() => handleNavItemClick("/gallery")} isActive={activeNavItem === "/gallery"}>gallery</NavItem>
+          <NavItem to="/menu" onClick={() => handleNavItemClick("/menu")} isActive={activeNavItem === "/menu"}>menu</NavItem>
+          <NavItem to="/faq" onClick={() => handleNavItemClick("/faq")} isActive={activeNavItem === "/faq"}>faq</NavItem>
+          <NavItem to="/contact" onClick={() => handleNavItemClick("/contact")} isActive={activeNavItem === "/contact"}>contact</NavItem>
         </ul>
-        <div  onClick={handleNav} className="menu-button">
-            {!nav ? <AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
+        <div onClick={handleNav} className="menu-button">
+          {!nav ? <AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
         </div>
-    </nav>
-    <nav className={`mobile-menu ${nav ? "clicked" : "hidden"}`}>
-      <ul>
-        <NavItem to="/" onClick={handleNav}>home</NavItem>
-        <NavItem to="/gallery" onClick={handleNav}>gallery</NavItem>
-        <NavItem to="/menu" onClick={handleNav}>menu</NavItem>
-        <NavItem to="/faq" onClick={handleNav}>faq</NavItem>
-        <NavItem to="/contact" onClick={handleNav}>contact</NavItem>
-      </ul>
-    </nav>
+      </nav>
+      <nav className={`mobile-menu ${nav ? "clicked" : "hidden"}`}>
+        <ul>
+          <NavItem to="/" onClick={() => handleNavItemClick("/")} isActive={activeNavItem === "/"}>home</NavItem>
+          <NavItem to="/gallery" onClick={() => handleNavItemClick("/gallery")} isActive={activeNavItem === "/gallery"}>gallery</NavItem>
+          <NavItem to="/menu" onClick={() => handleNavItemClick("/menu")} isActive={activeNavItem === "/menu"}>menu</NavItem>
+          <NavItem to="/faq" onClick={() => handleNavItemClick("/faq")} isActive={activeNavItem === "/faq"}>faq</NavItem>
+          <NavItem to="/contact" onClick={() => handleNavItemClick("/contact")} isActive={activeNavItem === "/contact"}>contact</NavItem>
+        </ul>
+      </nav>
     </>
   );
 };
