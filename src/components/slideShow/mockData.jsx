a893745bc15db41fc91b7a35e63866c2b15e8e5d
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2023-11-27 17:12:33 -0500

    adding scrolling effect in home page

---
+++ src/components/slideShow/mockData.jsx
@@ -0,0 +1,19 @@
+import pic1 from './../../images/slideShow/pic1.png';
+import pic2 from './../../images/slideShow/pic2.png';
+import pic3 from './../../images/slideShow/pic3.png';
+import pic4 from './../../images/slideShow/pic4.png';
+import pic5 from './../../images/slideShow/pic5.png';
+import pic6 from './../../images/slideShow/pic6.png';
+import pic7 from './../../images/slideShow/pic7.png';
+import pic8 from './../../images/slideShow/pic8.png';
+
+export const data = [
+    { id: 1, img: pic1 },
+    { id: 8, img: pic8 },
+    { id: 2, img: pic2 },
+    { id: 3, img: pic3 },
+    { id: 4, img: pic4 },
+    { id: 5, img: pic5 },
+    { id: 6, img: pic6 },
+    { id: 7, img: pic7 },
+];
--- src/components/slideShow/slideShow.jsx
+++ src/components/slideShow/slideShow.jsx
@@ -1,84 +1,22 @@
 import "./slideShow.css";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-
-// import Slider from "react-slick";
-import React, { Component } from "react";
-import Slider from "react-slick";
-
-function Arrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "black" }}
-      onClick={onClick}
-    />
-  );
-}
+import {data} from './mockData.jsx'
+import pic1 from './../../images/pic1.png'; 
 
 const SlideShow = () => {
-    var settings = {
-      dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      initialSlide: 0,
-      nextArrow: <Arrow />,
-      prevArrow: <Arrow />,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    };
   return (
-    <div className="cont">
-    <h2>Best Sellers</h2>
-    <Slider className="slider" {...settings}>
-      <div >
-        <h3>1</h3>
+    <div className="slide-show">
+      <h2>Best Sellers</h2>
+      <div className="slide-show-container">
+        <div id="slider">
+          {data.map(item => (
+          <img key={item.id} src={item.img} alt={`Image ${item.id}`} />
+        ))}
+        </div>
+        
       </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
-  </div>
+      <button>View More</button>
+    </div>
   )
 }
-export default SlideShow;
 
+export default SlideShow;
