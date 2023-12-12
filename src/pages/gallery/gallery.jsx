f0e0c943bc848f1f1dcc6432ce297dce2acfe204
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2023-12-12 15:49:39 -0500

    2 array filter method commented out

--- src/pages/gallery/gallery.jsx
+++ src/pages/gallery/gallery.jsx
@@ -2,6 +2,8 @@ import './gallery.css'
 import { cupcakeData, dholkiData, floralData, babyShowerData, charactersData, kidsData, signatureData, data } from './galleryData.jsx' 
 import { useState } from 'react';
 
+
+
 const filterItems = [
   { data: signatureData, text: "signature" },
   { data: babyShowerData, text: "baby showers" },
@@ -13,10 +15,23 @@ const filterItems = [
   { data: data, text: "etc" }
 ];
 
+const filterItems_1 = [
+  { data: signatureData, text: "signature" },
+  { data: babyShowerData, text: "baby showers" },
+  { data: floralData, text: "floral" },
+  { data: kidsData, text: "kids" }
+];
+
+const filterItems_2 = [
+  { data: charactersData, text: "characters" },
+  { data: dholkiData, text: "dholki" },
+  { data: cupcakeData, text: "cupcakes" },
+  { data: data, text: "etc" }
+];
+
 
 const Gallery = () => {
   const [filter, setFilter] = useState('signature');
-
   function handleFilter (new_filter) {
     setFilter(new_filter);
   }
@@ -29,9 +44,24 @@ const Gallery = () => {
         <h className="page_heading">Gallery</h>
       </div>
       <div>
-        <ul id="filter" className='text__style_3'>
-          {filterItems.map((item) => (<li onClick={()=>handleFilter(item.text)} 
-          className={item.text == filter ? "active" : ""} key={item.data}> {item.text} </li>))}
+        <ul id="filter" className='text__style_3 flex_center'>
+          {/* <div className="filter_top flex_center">
+            {filterItems_1.map((item) => (<li onClick={()=>handleFilter(item.text)} 
+            className={item.text == filter ? "active" : ""} key={item.data}> {item.text} </li>))}
+          </div>
+          <div className="filter_bottom flex_center">
+            {filterItems_2.map((item) => (<li onClick={()=>handleFilter(item.text)} 
+            className={item.text == filter ? "active" : ""} key={item.data}> {item.text} </li>))}
+          </div> */}
+                  {filterItems.map((item) => (
+          <li 
+            onClick={() => handleFilter(item.text)}
+            className={item.text === filter ? "active" : ""}
+            key={item.text} /* Assuming item.text is unique, otherwise use a different unique key */
+          >
+            {item.text}
+          </li>
+        ))}
         </ul>
       </div>
       <div id="gallery">
--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -2,7 +2,6 @@ import './menu.css'
 
 const Menu = () => {
   return (
-
       <div id="menu" className="col flex_center ">
         <div className="menu_hero flex_center col">
           <h className="page_heading">Menu</h>
@@ -11,62 +10,36 @@ const Menu = () => {
         <h2 className="header_line">Cake Flavors</h2>
         <hr className="line" />
         <div id="flavours" className="flex_center col">
-          <div className="flavor">
             <h3>Vanilla</h3>
-            <p>A classic comforting favorite with a timeless, comforting taste.</p>
-          </div>
-          <div className="flavor">
+            <p className='menu_text'>A classic comforting favorite with a timeless, comforting taste.</p>
             <h3>Chocolate</h3>
-            <p>Indulgent deep cocoa cake. Rich and decadent, a chocolate lover's dream.</p>
-          </div>
-          <div className="flavor">
+            <p className='menu_text'>Indulgent deep cocoa cake. Rich and decadent, a chocolate lover's dream.</p>
             <h3>Red Velvet</h3>
-            <p>Velvety, vibrant, with a subtle cocoa taste.</p>
-          </div>
-          <div className="flavor">
+            <p className='menu_text'>Velvety, vibrant, with a subtle cocoa taste.</p>
             <h3>Lemon</h3>
-            <p>Bright and zesty, a burst of citrusy delight.</p>
-          </div>
-          <div className="flavor">
+            <p className='menu_text'>Bright and zesty, a burst of citrusy delight.</p>
             <h3>Marble</h3>
-            <p>Swirls of vanilla and chocolate, the perfect blend in every bite.</p>
-          </div>
-          <div className="flavor">
+            <p className='menu_text'>Swirls of vanilla and chocolate, the perfect blend in every bite.</p>
             <h3>Confetti</h3>
-            <p>Colorful fun and festive, a celebration in every bite.</p>
-          </div>
-          <div className="flavor">
+            <p className='menu_text'>Colorful fun and festive, a celebration in every bite.</p>
             <h3>Mocha</h3>
-            <p>A coffee lovers delight, a balance of bitter and sweet for a sophisticated palate.</p>
-          </div>
+            <p className='menu_text'>A coffee lovers delight, a balance of bitter and sweet for a sophisticated palate.</p>
         </div>
         <h2 className="header_line">Buttercream Flavors</h2>
         <hr className="line" />
         <div id="flavours" className="flex_center col">  
-          <div className="flavor">
-            <h3>Salted Caramel</h3>
-            <p>A blissful blend of sweet caramel with a hint of sea salt.</p>
-          </div>
-          <div className="flavor">
-            <h3>Vanilla Swiss Meringue</h3>
-            <p>Silky and smooth, infused with a delicate vanilla essence.</p>
-          </div>
-          <div className="flavor">
-            <h3>Chocolate Fudge</h3>
-            <p>Rich and velvety, a chocolate lover's dream.</p>
-          </div>
-          <div className="flavor">
-            <h3>Cream Cheese</h3>
-            <p>Creamy and tangy, the perfect complement to any cake.</p>
-          </div>
-          <div className="flavor">
-            <h3>Hazelnut</h3>
-            <p>Nutty, creamy, and decadent, boasting the exquisite flavor of roasted hazelnuts.</p>
-          </div>
-          <div className="flavor">
-            <h3>Cookies and Cream</h3>
-            <p>A delightful blend of cookies and creamy goodness.</p>
-          </div>
+          <h3>Salted Caramel</h3>
+          <p className='menu_text'>A blissful blend of sweet caramel with a hint of sea salt.</p>
+          <h3>Vanilla Swiss Meringue</h3>
+          <p className='menu_text'>Silky and smooth, infused with a delicate vanilla essence.</p>
+          <h3>Chocolate Fudge</h3>
+          <p className='menu_text'>Rich and velvety, a chocolate lover's dream.</p>
+          <h3>Cream Cheese</h3>
+          <p className='menu_text'>Creamy and tangy, the perfect complement to any cake.</p>
+          <h3>Hazelnut</h3>
+          <p className='menu_text'>Nutty, creamy, and decadent, boasting the exquisite flavor of roasted hazelnuts.</p>
+          <h3>Cookies and Cream</h3>
+          <p className='menu_text'>A delightful blend of cookies and creamy goodness.</p>
         </div>
         <div className="menu_container">
           <h2 className="header__1">Cake Sizing Guide</h2>
