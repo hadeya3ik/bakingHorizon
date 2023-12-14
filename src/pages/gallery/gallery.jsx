ec5f03866a7aa20f9b5ed35606fbf016fa852494
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2023-12-13 20:53:19 -0500

    animation to gallery

--- src/pages/gallery/gallery.jsx
+++ src/pages/gallery/gallery.jsx
@@ -1,8 +1,11 @@
 import './gallery.css'
 import { cupcakeData, dholkiData, floralData, babyShowerData, charactersData, kidsData, signatureData, data } from './galleryData.jsx' 
 import { useState } from 'react';
-
-
+import Modal from 'react-modal'
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import CloseButton from 'react-bootstrap/CloseButton';
 
 const filterItems = [
   { data: signatureData, text: "signature" },
@@ -15,25 +18,19 @@ const filterItems = [
   { data: data, text: "etc" }
 ];
 
-const filterItems_1 = [
-  { data: signatureData, text: "signature" },
-  { data: babyShowerData, text: "baby showers" },
-  { data: floralData, text: "floral" },
-  { data: kidsData, text: "kids" }
-];
-
-const filterItems_2 = [
-  { data: charactersData, text: "characters" },
-  { data: dholkiData, text: "dholki" },
-  { data: cupcakeData, text: "cupcakes" },
-  { data: data, text: "etc" }
-];
-
-
 const Gallery = () => {
   const [filter, setFilter] = useState('signature');
-  function handleFilter (new_filter) {
-    setFilter(new_filter);
+  const [animate, setAnimate] = useState(true);
+
+  // sets the filter + animation
+  function handleFilter(new_filter) {
+    if (new_filter !== filter) { 
+      setAnimate(false); 
+      setTimeout(() => {
+        setFilter(new_filter); 
+        setAnimate(true);
+      }, 5); // Short timeout to ensure the class is re-applied
+    }
   }
 
   const filteredData = filterItems.filter((item) => item.text == filter)[0].data;
@@ -45,30 +42,20 @@ const Gallery = () => {
       </div>
       <div>
         <ul id="filter" className='text__style_3 flex_center'>
-          {/* <div className="filter_top flex_center">
-            {filterItems_1.map((item) => (<li onClick={()=>handleFilter(item.text)} 
-            className={item.text == filter ? "active" : ""} key={item.data}> {item.text} </li>))}
-          </div>
-          <div className="filter_bottom flex_center">
-            {filterItems_2.map((item) => (<li onClick={()=>handleFilter(item.text)} 
-            className={item.text == filter ? "active" : ""} key={item.data}> {item.text} </li>))}
-          </div> */}
-                  {filterItems.map((item) => (
-          <li 
-            onClick={() => handleFilter(item.text)}
-            className={item.text === filter ? "active" : ""}
-            key={item.text} /* Assuming item.text is unique, otherwise use a different unique key */
-          >
-            {item.text}
-          </li>
-        ))}
+          {filterItems.map((item) => (<li onClick={()=>handleFilter(item.text)} 
+          className={item.text == filter ? "active" : ""} key={item.data}> {item.text} </li>))}
         </ul>
       </div>
       <div id="gallery">
-        {filteredData.map(item => (
-          <img key={item.id} src={item.img} alt={`Image ${item.id}`} />
-        ))}
-      </div>
+  {filteredData.map(item => (
+    <img
+      key={item.id}
+      src={item.img}
+      alt={`Image ${item.id}`}
+      className={animate ? 'animate' : ''}
+    />
+  ))}
+</div>
     </div>
   );
 }
