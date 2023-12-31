ca5c6a99a2d4cedc7ffddb8a30d4e431b21d4b40
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2023-12-31 02:37:17 -0500

    updating import scripts to genberate jsx file

--- src/scripts/getImports.sh
+++ src/scripts/getImports.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
 
+# output_file="./test.txt"
+
+# for dir in ../images/dec31gallery/*/; do
+#     (cd "$dir" && 
+#         for file in *; do
+#             echo "import" $(basename $file .jpg) "from ./../../images/gallery/"$dir$file  >> $output_file
+#         done)
+#     echo
+# done
+
+# counter=0
+# for dir in ../images/dec31gallery/*/; do 
+#     echo "export const "$(basename $dir)" = ["  >> $output_file
+#     (cd $dir && for file in * ; do
+#         baseFile=$(basename $file .jpg)
+#         echo "{img: $baseFile.jpg, img_hd : $baseFile.webp, key: $counter} , "  >> $output_file
+#         ((counter++))
+#         done)
+#     echo "]"  >> $output_file
+# done
+
+
+output_file="./test.txt"
+# echo "jlk" > "$output_file" # Clear the output file at the beginning
+echo "" > "$output_file"
+
 for dir in ../images/dec31gallery/*/; do
-    (cd "$dir" && 
-        for file in *; do
-            echo "import" $(basename $file .jpg) "from ./../../images/gallery/"$dir$file
-        done)
-    echo
+        (cd "$dir" && 
+            for file in *; do
+            echo  >> "$output_file" 
+                if [ "${file##*.}" = "webp" ]; then
+                    echo "import $(basename "$file" .webp) from ./../../images/gallery/${dir}${file}" >> "$output_file" 
+                else 
+                    echo "import $(basename "$file" .jpg)_hd from ./../../images/gallery/${dir}${file}" >> "$output_file" 
+                fi
+            done)
+    echo  >> "$output_file" 
 done
 
-counter=0
-for dir in ../images/dec31gallery/; do 
-    echo "export const "$(basename $dir)" = ["
-    (cd $dir && for file in * ; do
-        baseFile=$(basename $file .jpg)
-        echo "{img: $baseFile.jpg, img_hd : $baseFile.webp, key: $counter} , "
-        ((counter++))
-        done)
-    echo "]"
-done
+
+# counter=0
+# for dir in ../images/dec31gallery/*/; do 
+#     echo "export const "$(basename $dir)" = [" 
+#     (cd $dir && for file in * ; do
+#         baseFile=$(basename $file .jpg)
+#         echo "{img: $baseFile.jpg, img_hd : $baseFile.webp, key: $counter} , "  
+#         ((counter++))
+#         done)
+#     echo "]"  
+# done
