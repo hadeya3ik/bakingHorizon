adeb70b662aeee35ea02b6078a641cf4b953a4d0
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2024-01-04 00:59:44 -0500

    updating borwserRouter base name adding /

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import {BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={'bakingHorizon'}>
+    <BrowserRouter basename={'bakingHorizon/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
