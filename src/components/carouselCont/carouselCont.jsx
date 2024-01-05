a5fff383d7043f60de2bb138cbdbd660d4ffebef
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2024-01-04 22:03:20 -0500

    cropping carousel photos

--- src/components/carouselCont/carouselCont.jsx
+++ src/components/carouselCont/carouselCont.jsx
@@ -4,20 +4,20 @@ import Carousel from 'react-bootstrap/Carousel';
 import img1 from './../../images/carousel/lion_boy.jpg';
 import img2 from '../../images/carousel/donuts.jpg'
 import img3 from '../../images/carousel/moon_bear.jpg'
-import img4 from '../../images/carousel/winnie_poo.jpg'
+import img4 from '../../images/carousel/astro.png'
 
 const images = [
   { img: img1, alt : "lion boy" },
   { img: img2, alt : "donuts"},
   { img: img3, alt : "bear"},
-  { img: img4, alt : "winnie"}
+  { img: img4, alt : "astro"}
 ];
 
 function CarouselCont() {
   return (
     <Carousel className="custom-carousel">
       {images.map((item, index) => (
-        <Carousel.Item key={index} interval={1000}>
+        <Carousel.Item key={index} interval={2000}>
           <img src={item.img} alt={item.alt} className="d-block w-100" />
         </Carousel.Item>
       ))}
